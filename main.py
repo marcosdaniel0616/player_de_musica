6cfac6e02c264440977a134c32fee0311a332e24
Marcos Daniel <marcos.daniel0616@gmail.com>
2021-07-22 18:47:24 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from playsound import playsound
+from os import chdir, getcwd, listdir
+from random import randrange
+
+cam = input('Digite o caminho: ')
+
+chdir(cam)
+print(getcwd())
+
+for letra in cam:
+    if letra == '\\':
+        cam = cam.replace('\\', '/')
+
+musicas = []
+for indice, c in enumerate(listdir()):
+    print([indice], c)
+    musicas.append(c)
+else:
+    print([indice + 1], 'Aleatória')
+
+musica = int(input('Informe o indice da música que você quer: '))
+
+if musica >= int(len(musicas)):
+    aleatorio = randrange(len(musicas))
+    aleatorio = musicas[aleatorio]
+    musica = aleatorio
+else:
+    musica = musicas[musica]
+
+playsound(f'{cam + "/" + musica}')
